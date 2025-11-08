9359e23d19d83a15bade665fff6fdb1e26fbea8b
JanierII <janieer15@gmail.com>
2025-11-08 15:03:41 -0500

    AutoMapper  and DTO's created

---
+++ APIW.Movies/DAL/Models/Dtos/CategoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace APIW.Movies.DAL.Models.Dtos
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la categoria es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El numuero maximo de caracteres es de 100.")]
+
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public  DateTime ModifiedDate { get; set; }
+    }
+}
